154969701e2ba28b7c4069d183e93f951258ad29
baris <barisakbas54@gmail.com>
2024-03-01 02:04:54 +0100

    geliyoruzGithub

---
+++ src/P01.java
@@ -0,0 +1,12 @@
+public class P01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a=120;
+        int b=80;
+        int c;
+
+        c= a+b;
+        System.out.println("c = " + c);
+    }
+}
+
